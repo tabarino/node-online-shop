2e64421dd92079b68304da165081bb22620b6036
Ivan Tabarino <tabarino@outlook.com>
2022-03-26 13:06:25 +0000

    feat: keep cart items updated

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -1,3 +1,5 @@
+const Product = require('./product.model');
+
 class Cart {
   constructor (items = [], totalQuantity = 0, totalPrice = 0) {
     this.items = items;
@@ -5,12 +7,42 @@ class Cart {
     this.totalPrice = totalPrice;
   }
 
+  async updatePrices () {
+    const productIds = this.items.map(item => item.product.id);
+    const products = await Product.findMultiple(productIds);
+    const deletableCartItemProductIds = [];
+
+    for (const cartItem of this.items) {
+      const product = products.find(prod => prod.id === cartItem.product.id);
+
+      if (!product) {
+        deletableCartItemProductIds.push(cartItem.product.id);
+        continue;
+      }
+
+      cartItem.product = product;
+      cartItem.totalPrice = cartItem.quantity * cartItem.product.price;
+    }
+
+    if (deletableCartItemProductIds.length > 0) {
+      this.items = this.items.filter(item => deletableCartItemProductIds.indexOf(item.product.id) < 0);
+    }
+
+    this.totalQuantity = 0;
+    this.totalPrice = 0;
+
+    for (const item of this.items) {
+      this.totalQuantity = this.totalQuantity + item.quantity;
+      this.totalPrice = this.totalPrice + item.totalPrice;
+    }
+  }
+
   addItem (product) {
     const cartItem = {
       product: product,
       quantity: 1,
       totalPrice: product.price
-    }
+    };
 
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
